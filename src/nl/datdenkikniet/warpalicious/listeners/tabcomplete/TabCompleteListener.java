9655ba174a247d859041d78cfcc90ab885472082
Jona123 <jcdra1@gmail.com>
2020-03-13 22:28:19 +0100

    Add warp tab completion

--- src/nl/datdenkikniet/warpalicious/listeners/tabcomplete/TabCompleteListener.java
+++ src/nl/datdenkikniet/warpalicious/listeners/tabcomplete/TabCompleteListener.java
@@ -1,6 +1,7 @@
 package nl.datdenkikniet.warpalicious.listeners.tabcomplete;
 
 import nl.datdenkikniet.warpalicious.WarpaliciousPlugin;
+import nl.datdenkikniet.warpalicious.handling.WarpHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.TabCompleteEvent;
@@ -8,21 +9,26 @@ import org.bukkit.event.server.TabCompleteEvent;
 public abstract class TabCompleteListener implements Listener {
 
   protected WarpaliciousPlugin plugin;
+  protected WarpHandler handler;
 
   public TabCompleteListener(WarpaliciousPlugin instance) {
     this.plugin = instance;
+    this.handler = this.plugin.getWarpHandler();
   }
 
   @EventHandler
-  public void tabCmplt(TabCompleteEvent evt){
+  public void tabCmplt(TabCompleteEvent evt) {
     String buffer = evt.getBuffer();
-    if (buffer.startsWith("/" + getCommandName())){
-      complete(evt);
+    if (buffer.startsWith("/" + getCommandName())) {
+      String[] bufSplit = buffer.split(" ");
+      String[] args = new String[bufSplit.length - 1];
+      System.arraycopy(bufSplit, 1, args, 0, bufSplit.length - 1);
+      complete(evt, args);
     }
   }
 
   protected abstract String getCommandName();
 
-  public abstract void complete(TabCompleteEvent evt);
+  public abstract void complete(TabCompleteEvent evt, String[] args);
 
 }
--- src/nl/datdenkikniet/warpalicious/listeners/tabcomplete/WarpTabCompleteListener.java
+++ src/nl/datdenkikniet/warpalicious/listeners/tabcomplete/WarpTabCompleteListener.java
@@ -1,6 +1,10 @@
 package nl.datdenkikniet.warpalicious.listeners.tabcomplete;
 
+import java.util.ArrayList;
+import java.util.List;
 import nl.datdenkikniet.warpalicious.WarpaliciousPlugin;
+import nl.datdenkikniet.warpalicious.handling.Warp;
+import org.bukkit.entity.Player;
 import org.bukkit.event.server.TabCompleteEvent;
 
 public class WarpTabCompleteListener extends TabCompleteListener {
@@ -15,7 +19,26 @@ public class WarpTabCompleteListener extends TabCompleteListener {
   }
 
   @Override
-  public void complete(TabCompleteEvent evt) {
-
+  public void complete(TabCompleteEvent evt, String[] args) {
+    List<String> completions = new ArrayList<>();
+    evt.setCompletions(completions);
+    if (evt.getSender() instanceof Player) {
+      Player player = (Player) evt.getSender();
+      List<Warp> warps = handler.getWarpList(player);
+      final List<Warp> filteredWarps = new ArrayList<>();
+      if (args.length == 1) {
+        warps.iterator().forEachRemaining((warp) -> {
+          if (warp.getName().toLowerCase().startsWith(args[0].toLowerCase())) {
+            filteredWarps.add(warp);
+          }
+        });
+      } else {
+        filteredWarps.addAll(warps);
+      }
+      System.out.println(filteredWarps.size());
+      if (filteredWarps.size() != 0) {
+        filteredWarps.forEach((warp) -> completions.add(warp.getName()));
+      }
+    }
   }
 }
